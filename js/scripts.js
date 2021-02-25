e4af69dec5443ce92af89008b5dcc3478a34e152
Nakia Callaway <nik.callaway@outlook.com>
2021-02-24 20:51:54 -0800

    day 2 daily challenge

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let str = "Supercalifragilisticexpialidocious";
+let n = str.length;
+const bigint = 12345678901234567890123456789012345678901234567890n;
+
+
+console.log(n)
+
+console.log(27)
+
+console.log(bigint)
+
